9b2b2a2e636cbc84f2cadaba1a16f9954b50914f
Frances Wang <franceswang068@gmail.com>
2017-03-20 00:51:09 -0400

    Execute the getQuote function once the page is loaded

--- script.js
+++ script.js
@@ -1,27 +1,30 @@
+const API_URL = "https://crossorigin.me/http://api.forismatic.com/api/1.0/";
+
 $( document ).ready( readyFn );
 
 function readyFn(jQuery) {
 
-	const API_URL = "https://crossorigin.me/http://api.forismatic.com/api/1.0/";
+	getQuote();
 
-	$("#getQuote").on("click", function(){
+	$(".new-btn").on("click", getQuote);
 
-		var jqxhr = $.ajax({
-			url: API_URL,
-			dataType: "json",
-			data: {
-				method: "getQuote",
-				format: "json",
-				lang: "en",
-			}
-		})
-			.done(function(data) {
-				console.log(JSON.stringify(data));
-				var $quote = data.quoteText;
-				var $author = data.quoteAuthor;
-				$("#quote").text($quote);
-				$("#author").text($author);
-			})
-	});
 };
 
+function getQuote() {
+	var jqxhr = $.ajax({
+		url: API_URL,
+		dataType: "json",
+		data: {
+			method: "getQuote",
+			format: "json",
+			lang: "en"
+		}
+	})
+		.done(function(data) {
+			// console.log(JSON.stringify(data));
+			var $quote = data.quoteText;
+			var $author = data.quoteAuthor;
+			$(".quote-text").text($quote);
+			$(".quote-author").text("- " + $author);
+		})
+};
